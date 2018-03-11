ea017de0a63b971f6a913709f3c6def192f10c7e
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2018-03-11 23:07:50 +0800

    continuous-integration/Install Build Dependencies.bash - 安裝建構 PDF 版用的思源黑體
    假定大家都不看紙本了
    
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

--- continuous-integration/Install Build Dependencies.bash
+++ continuous-integration/Install Build Dependencies.bash
@@ -9,6 +9,50 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+## Runtime Dependencies Checking
+declare\
+	runtime_dependency_checking_result='still-pass' \
+	required_software
+
+for required_command in \
+	basename\
+	dirname\
+	fc-cache\
+	realpath\
+	wget; do
+	if ! command -v "${required_command}" &>/dev/null; then
+		runtime_dependency_checking_result='fail'
+
+		case "${required_command}" in
+			basename\
+			|dirname\
+			|realpath)
+				required_software='GNU Coreutils'
+				;;
+			fc-cache)
+				required_software='Fontconfig'
+				;;
+			wget)
+				required_software='GNU Wget'
+				;;
+			*)
+				required_software="${required_command}"
+				;;
+		esac
+
+		printf --\
+			"Error: This program requires \"%s\" to be installed and it's executables in the executable searching paths.\n"\
+			"${required_software}" 1>&2
+		unset required_software
+	fi
+done; unset required_command required_software
+
+if [ "${runtime_dependency_checking_result}" = 'fail' ]; then
+	printf --\
+		"Error: Runtime dependency checking fail, the progrom cannot continue.\n" 1>&2
+	exit 1
+fi; unset runtime_dependency_checking_result
+
 ## Non-overridable Primitive Variables
 ## BASHDOC: Shell Variables » Bash Variables
 ## BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters
@@ -26,6 +70,8 @@ if [ -v "BASH_SOURCE[0]" ]; then
 fi
 declare -ar RUNTIME_COMMANDLINE_PARAMETERS=("${@}")
 
+declare CACHE_BASE_DIRECTORY="${HOME}/build-cache"
+
 ## init function: entrypoint of main program
 ## This function is called near the end of the file,
 ## with the script's command-line parameters as arguments
@@ -39,11 +85,21 @@ init(){
 		exit 1
 	fi
 
+	if ! test -d "${CACHE_BASE_DIRECTORY}"; then
+		mkdir\
+			"${CACHE_BASE_DIRECTORY}"
+	fi
+
+	install_omegat
+	install_source_han_sans
+	
+}; declare -fr init
+
+install_omegat(){
 	# FIXME: Hardcoded download URL!
 	# TODO: Proper latest release download URL parsing
 	declare -r\
-		OMEGAT_RELEASE_ARCHIVE_DIRECT_DOWNLOADABLE_URL='https://sourceforge.net/projects/omegat/files/OmegaT%20-%20Standard/OmegaT%203.6.0%20update%208/OmegaT_3.6.0_08_Without_JRE.zip'\
-		CACHE_BASE_DIRECTORY="${HOME}/build-cache"
+		OMEGAT_RELEASE_ARCHIVE_DIRECT_DOWNLOADABLE_URL='https://sourceforge.net/projects/omegat/files/OmegaT%20-%20Standard/OmegaT%203.6.0%20update%208/OmegaT_3.6.0_08_Without_JRE.zip'
 
 	declare omegat_release_archive_basename="$(
 		basename\
@@ -57,18 +113,13 @@ init(){
 		printf --\
 			"%s\n"\
 			"INFO: Existing OmegaT installation detected, installation skipped."
-		exit 0
-	fi
-
-	if ! test -d "${CACHE_BASE_DIRECTORY}"; then
-		mkdir\
-			"${CACHE_BASE_DIRECTORY}"
+		return 0
 	else
 		# 清空快取目錄中的 OmegaT 舊版本安裝（如果有）
 		rm\
 			--recursive\
 			--force\
-			"${CACHE_BASE_DIRECTORY:?}/"*
+			"${CACHE_BASE_DIRECTORY:?}"/OmegaT_*
 	fi
 	
 	# FIXME: 3.x 不具獨立前綴目錄故自行建立之
@@ -78,6 +129,7 @@ init(){
 	unzip\
 		"${omegat_release_archive_basename}.zip"\
 		-d "${omegat_installation_prefix}"
+	rm "${omegat_release_archive_basename}.zip"
 
 	# Check installation
 	if ! test -f "${omegat_installation_prefix}/OmegaT.jar"; then
@@ -85,11 +137,51 @@ init(){
 			'%s: Error: Installation verification failed.\n'\
 			"${RUNTIME_EXECUTABLE_NAME}"\
 			1>&2
-		exit 1
+		return 1
 	else
-		exit 0
+		return 0
 	fi
-}; declare -fr init
+}; declare -fr install_omegat
+
+install_source_han_sans(){
+	local -r download_url='https://github.com/adobe-fonts/source-han-sans/blob/release/OTF/SourceHanSansTC.zip?raw=true'
+	local download_basename="$(basename --suffix='?raw=true' "${download_url}")"
+	local download_basename_without_suffix="$(basename --suffix='.zip' "${download_basename}")"
+	local -r download_basename download_basename_without_suffix
+
+	local -r install_prefix="${CACHE_BASE_DIRECTORY}/${download_basename_without_suffix}"
+
+	local -r user_local_font_dir="${HOME}/.local/share/fonts"
+
+	if test -d "${install_prefix}"; then
+		printf --\
+			"%s\n"\
+			"INFO: Existing Source Han Sans download detected, download skipped."
+	else
+		wget\
+			--output-document="${download_basename}"\
+			--directory-prefix="${CACHE_BASE_DIRECTORY}"\
+			--no-clobber\
+			"${download_url}"
+		unzip\
+			"${download_basename}"\
+			-d "${CACHE_BASE_DIRECTORY}"
+		rm "${download_basename}"
+	fi
+
+	# setup
+	mkdir\
+		--parents\
+		"${user_local_font_dir}"
+
+	cp\
+		--recursive\
+		"${install_prefix}"\
+		"${user_local_font_dir}"
+
+	fc-cache --verbose "${user_local_font_dir}"
+	return 0
+}; declare -fr install_source_han_sans
 
 ## Traps: Functions that are triggered when certain condition occurred
 ## Shell Builtin Commands » Bourne Shell Builtins » trap
@@ -99,9 +191,6 @@ trap_errexit(){
 }; declare -fr trap_errexit; trap trap_errexit ERR
 
 trap_exit(){
-	rm\
-		--force\
-		"${omegat_release_archive_basename}.zip"
 
 	return 0
 }; declare -fr trap_exit; trap trap_exit EXIT
